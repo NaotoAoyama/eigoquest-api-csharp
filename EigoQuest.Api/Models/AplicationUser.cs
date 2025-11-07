6d0e6917c954d5b07d91763da5516e3fa2e1db7f
NaotoAoyama <naoto.aoyama051217@gmail.com>
2025-11-07 19:23:43 +0900

    Initial commit C# API Project

---
+++ EigoQuest.Api/Models/AplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace EigoQuest.Api.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // 将来的にカスタムフィールド（例：レベル、経験値など）
+        // を追加する場合はここに記述
+    }
+}
